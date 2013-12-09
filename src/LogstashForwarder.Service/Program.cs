18acb80dc81f19ddec6bfa8d2e2d6f3de49ae1e3
David Laing <david@davidlaing.com>
2013-12-09 10:17:49 +0000

    Create LogstashForwarder.Service

--- src/LogstashForwarder.Service/Program.cs
+++ src/LogstashForwarder.Service/Program.cs
@@ -1,4 +1,6 @@
 using System;
+using LogstashForwarder.Core;
+using Topshelf;
 
 namespace LogstashForwarder.Service
 {
@@ -6,7 +8,48 @@ namespace LogstashForwarder.Service
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+		    log4net.Config.XmlConfigurator.Configure();
+
+            HostFactory.Run(x =>
+            {
+                x.Service<LogstashForwarderService>(s =>
+                {
+                    s.ConstructUsing(name => new LogstashForwarderService());
+                    s.WhenStarted(tc =>
+                    {
+                        tc.Start();
+                    });
+                    s.WhenStopped(tc => tc.Stop());
+                });
+                x.RunAsNetworkService();
+                x.StartAutomatically();
+
+                x.SetDescription("LogStash Forwarder.NET - forwards (Windows) log files to Logsearch cluster");
+                x.SetDisplayName("LogStash Forwarder.NET");
+                x.SetServiceName("logstash_forwarder_net");
+
+                x.EnableServiceRecovery(rc =>
+                {
+                    rc.RestartService(1); // restart the service after 1 minute
+                });
+
+                x.UseLog4Net();
+            });
 		}
 	}
+
+    public class LogstashForwarderService
+    {
+        private LogstashForwarderProcessManager _logstashForwarderProcessManager;
+        public void Start()
+        {
+            _logstashForwarderProcessManager = new LogstashForwarderProcessManager();
+            _logstashForwarderProcessManager.Start();
+        }
+
+        public void Stop()
+        {
+            _logstashForwarderProcessManager.Stop();
+        }
+    }
 }
