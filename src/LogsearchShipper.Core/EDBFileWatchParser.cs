c341e3c7858cbbcf58e485fe369899f803689b01
David Laing <david@davidlaing.com>
2014-06-26 17:11:01 +0100

    Add source.host field for EDB log files

--- src/LogsearchShipper.Core/EDBFileWatchParser.cs
+++ src/LogsearchShipper.Core/EDBFileWatchParser.cs
@@ -78,7 +78,7 @@ namespace LogsearchShipper.Core
 				foreach (var service in server.Services)
 				{
 					var fields = new FieldCollection();
-					fields.Add(new FieldElement {Key = "shipper.host", Value = Environment.MachineName});
+					fields.Add(new FieldElement {Key = "host", Value = server.Name});
 					fields.Add(new FieldElement {Key = "service", Value = service.Name});
 					foreach (FieldElement field in _environmentWatchElement.Fields)
 					{
