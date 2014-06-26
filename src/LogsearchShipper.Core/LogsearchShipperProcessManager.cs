819acf7dd7bfba48178ecf395a3253de11fc8762
David Laing <david@davidlaing.com>
2014-06-26 18:06:48 +0100

    Increase nxlog DirCheckInterval to 30 seconds - significantly reduces CPU load by only scanning for new files every 30 seconds

--- src/LogsearchShipper.Core/LogsearchShipperProcessManager.cs
+++ src/LogsearchShipper.Core/LogsearchShipperProcessManager.cs
@@ -225,6 +225,7 @@ SpoolDir %ROOT%\data
 		///         ReadFromLast TRUE
 		///         SavePos	TRUE
 		///         CloseWhenIdle TRUE
+		///         DirCheckInterval 30
 		///         Exec	$path = file_name(); $type = "myfile_type"; $field1="field1 value"; $field2="field2 value" $Message =
 		///         $raw_event;
 		///     </Input>
@@ -251,6 +252,7 @@ SpoolDir %ROOT%\data
 	ReadFromLast TRUE
 	SavePos	TRUE
 	CloseWhenIdle TRUE
+	DirCheckInterval 30
 	Exec	$path = file_name(); $name = ""logsearch-shipper.NET""; $module = ""nxlog""; $type = ""{2}""; ",
 					i,
 					watch.Files.Replace(@"\",@"\\"),
