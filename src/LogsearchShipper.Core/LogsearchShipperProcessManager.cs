a4ede254c59cb9c246ec2bd4af81844fea9acdec
David Laing <david@davidlaing.com>
2014-06-27 11:53:06 +0100

    Limit maximum message size to just less than 1MB; or NXLog dies

--- src/LogsearchShipper.Core/LogsearchShipperProcessManager.cs
+++ src/LogsearchShipper.Core/LogsearchShipperProcessManager.cs
@@ -264,7 +264,8 @@ SpoolDir %ROOT%\data
 				{
 					filesSection += string.Format(@"${0} = ""{1}""; ", field.Key, field.Value);
 				}
-				filesSection += @"$Message = $raw_event;
+				// Limit maximum message size to just less than 1MB; or NXLog dies
+				filesSection += @"$Message = substr($raw_event, 0, 1048542);
 </Input>
 ";
 			}
