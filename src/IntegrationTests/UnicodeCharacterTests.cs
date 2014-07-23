fb37d0c0b1594ff544699872ee15e838137ad5f3
David Laing <david@davidlaing.com>
2014-07-23 14:50:00 +0100

    Write some super simple tests that ship a single log event

---
+++ src/IntegrationTests/UnicodeCharacterTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Management;
+using NUnit.Framework;
+
+namespace IntegrationTests
+{
+	[TestFixture]
+	public class UnicodeCharacterTests
+	{
+		private static string Log4NetLogFilePath = Environment.CurrentDirectory +
+		                                           @"\Log4NetLogContainingUnicodeCharacters.log";
+		[Test]
+		public void ShouldSuccessfullyLog4NetLogContainingUnicodeCharacters()
+		{
+
+			Process shipper = null;
+
+			File.WriteAllText(Log4NetLogFilePath,	string.Empty);
+
+			File.Delete("LogSearchShipper.exe.config.bak");
+			File.Move("LogSearchShipper.exe.config", "LogSearchShipper.exe.config.bak");
+			File.Move("LogSearchShipper.exe.config.ShouldSuccessfullyShipLog4NetLogContainingUnicodeCharacters", "LogSearchShipper.exe.config");
+			try
+			{
+				shipper = Utils.StartProcess(Environment.CurrentDirectory + @"\LogSearchShipper.exe",
+					"-instance:ShouldSuccessfullyShipLog4NetLogContainingUnicodeCharacters");
+
+				System.Threading.Thread.Sleep(TimeSpan.FromSeconds(30));
+
+				var nowInBST = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time")).ToString("yyyy-MM-dd HH:mm:ss,fff");
+				File.WriteAllLines(Log4NetLogFilePath, new[]{nowInBST + @" [112] DEBUG CityIndexGeneric.Logic.Utilities.LogWriter [(null)] - ContentData Id:38654759125 de-DE 'CFDs ֠eine Alternative zu Zertifikaten oder ETFs' cached until 2014-07-22 12:28:45."});
+
+				System.Threading.Thread.Sleep(TimeSpan.FromSeconds(60));
+
+			}
+			finally
+			{
+				Utils.ShutdownProcess(shipper);
+
+				File.Delete("LogSearchShipper.exe.config");
+				File.Move("LogSearchShipper.exe.config.bak", "LogSearchShipper.exe.config");
+			}
+
+		}
+
+	}
+}
