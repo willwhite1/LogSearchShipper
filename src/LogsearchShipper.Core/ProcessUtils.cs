3e11665074eeffeec3ef928dbe35360f83bbdacf
fandrei <andrei.faber@gmail.com>
2014-09-03 14:50:01 +0700

    make nxlog service restart when it crashed

--- src/LogsearchShipper.Core/ProcessUtils.cs
+++ src/LogsearchShipper.Core/ProcessUtils.cs
@@ -8,7 +8,7 @@ namespace LogSearchShipper.Core
 {
 	public static class ProcessUtils
 	{
-		public static Process StartProcess(string processPath, string processArgs)
+		public static Process StartProcess(string processPath, string processArgs, Action<string> logOutput = null)
 		{
 			var process = new Process
 			{
@@ -23,16 +23,42 @@ namespace LogSearchShipper.Core
 					RedirectStandardInput = true,
 				}
 			};
-			process.OutputDataReceived += (sender, args) =>
-				Trace.WriteLine(string.Format("{0}: {1}", processPath, args.Data));
-			process.ErrorDataReceived += (sender, args) =>
-				Trace.WriteLine(string.Format("{0}: {1}", processPath, args.Data));
+
+			if (logOutput == null)
+				logOutput = DebugTrace;
+			process.OutputDataReceived +=
+				(sender, args) =>
+				{
+					var message = string.Format("{0}: {1}", processPath, args.Data);
+					logOutput(message);
+				};
+			process.ErrorDataReceived +=
+				(sender, args) =>
+				{
+					var message = string.Format("{0}: {1}", processPath, args.Data);
+					logOutput(message);
+				};
 
 			process.Start();
 			process.BeginOutputReadLine();
 			process.BeginErrorReadLine();
 
 			return process;
+		}
+
+		static void DebugTrace(string message)
+		{
+			Trace.WriteLine(message);
+		}
+
+		public static string Execute(string processPath, string processArgs)
+		{
+			var buf = new StringBuilder();
+			var process = StartProcess(processPath, processArgs,
+				message => buf.AppendLine(message));
+			if (!process.WaitForExit(10 * 1000) || process.ExitCode != 0)
+				throw new ApplicationException(buf.ToString());
+			return buf.ToString();
 		}
 	}
 }
