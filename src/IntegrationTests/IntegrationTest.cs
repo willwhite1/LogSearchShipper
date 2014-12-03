e61245341078165d8415bb20dd85b24bb9cb5362
Andrei Faber <andrei.faber@gmail.com>
2014-12-03 11:22:18 +0700

    refactoring

--- src/IntegrationTests/IntegrationTest.cs
+++ src/IntegrationTests/IntegrationTest.cs
@@ -6,52 +6,17 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 
-using LogSearchShipper.Core;
 using NUnit.Framework;
 
 namespace IntegrationTests
 {
 	[TestFixture]
-	class IntegrationTest
+	class IntegrationTest : IntegrationTestBase
 	{
 		void Init()
 		{
 			_currentGroupId = Guid.NewGuid().ToString();
-
-			if (_initDone)
-				return;
-
-			_basePath = Path.Combine(Environment.CurrentDirectory, "LogSearchShipper.Test");
-			if (!Directory.Exists(_basePath))
-				Directory.CreateDirectory(_basePath);
-
-			Utils.Cleanup(_basePath);
-			Directory.CreateDirectory(LogsPath);
-
-			var exeFile = "LogsearchShipper.exe";
-			var exeFileCopy = Path.Combine(_basePath, exeFile);
-			File.Copy(exeFile, exeFileCopy);
-
-			File.Copy("LogsearchShipper.exe.config.Test", Path.Combine(_basePath, "LogsearchShipper.exe.config"));
-
-			foreach (var file in Directory.GetFiles(Environment.CurrentDirectory, "*.dll"))
-			{
-				var newFile = Path.Combine(_basePath, Path.GetFileName(file));
-				File.Copy(file, newFile);
-			}
-			_exePath = exeFileCopy;
-
-			StartShipperService();
-
-			_initDone = true;
-
-			Utils.WriteDelimiter();
-		}
-
-		[TestFixtureTearDown]
-		public void TearDown()
-		{
-			StopShipperService();
+			InitCommon();
 		}
 
 		[Test]
@@ -243,45 +208,9 @@ namespace IntegrationTests
 			Trace.WriteLine("Success");
 		}
 
-		void StartShipperService()
-		{
-			Trace.WriteLine("    Starting the shipper service");
-
-			_shipperProcess = ProcessUtils.StartProcess(_exePath, "-instance:OverallTest");
-
-			Trace.WriteLine("    Waiting 30 seconds for shipper to startup...");
-			Thread.Sleep(TimeSpan.FromSeconds(30));
-		}
-
-		void StopShipperService()
-		{
-			Utils.WriteDelimiter();
-			Trace.WriteLine("    Stopping the shipper service");
-
-			Utils.ShutdownProcess(_shipperProcess);
-			_shipperProcess = null;
-		}
-
-		string GetTestPath(string testName)
-		{
-			var res = Path.Combine(LogsPath, testName);
-			if (!Directory.Exists(res))
-				Directory.CreateDirectory(res);
-			return res;
-		}
-
-		string LogsPath { get { return Path.Combine(_basePath, "logs"); } }
-
 		private string _currentGroupId;
-
-		private string _basePath;
-
-		private string _exePath;
-		private Process _shipperProcess;
 
 		private const int LinesPerFile = 100;
 		private const int LogFilesCount = 10;
-
-		private bool _initDone;
 	}
 }
