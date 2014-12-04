8cdc1cc74e57a48de875f891d931af1f844b1e89
Andrei Faber <andrei.faber@gmail.com>
2014-12-04 14:00:30 +0700

    developing EDB logging integration test

---
+++ src/IntegrationTests/EdbLoggingTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+
+namespace IntegrationTests
+{
+	[TestFixture]
+	class EdbLoggingTest : IntegrationTestBase
+	{
+		void Init()
+		{
+			Init("LogSearchShipper.EdbLoggingTest", "LogsearchShipper.exe.config.EdbLoggingTest");
+		}
+
+		[Test]
+		public void TestEdbLogging()
+		{
+			Init();
+		}
+	}
+}
