807ff2d34c8c343648456c5b6fc53af8795af8df
fandrei <andrei.faber@gmail.com>
2015-01-05 11:10:17 +0700

    cleanup ConsoleAppender configs

---
+++ src/LogSearchShipper/Log4net/DefaultConsoleAppender.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using log4net.Appender;
+using log4net.Core;
+using log4net.Filter;
+using log4net.Layout;
+
+namespace LogSearchShipper.Log4net
+{
+	// DEBUG is too verbose to see what is going on in the console
+	public sealed class DefaultConsoleAppender : ConsoleAppender
+	{
+		public override void ActivateOptions()
+		{
+			if (Layout == null)
+			{
+				var layout = new PatternLayout("%utcdate{ISO8601} [%thread] %-5level %logger - %.255message%newline");
+				layout.ActivateOptions();
+				Layout = layout;
+			}
+
+			if (FilterHead == null)
+			{
+				AddFilter(new LevelRangeFilter { LevelMin = Level.Info, LevelMax = Level.Fatal, });
+			}
+
+			base.ActivateOptions();
+		}
+	}
+}
