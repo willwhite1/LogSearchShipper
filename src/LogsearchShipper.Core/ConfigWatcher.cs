99635c01287f99cd62c24fe70a94b01f8026baf9
fandrei <andrei.faber@gmail.com>
2015-02-11 15:53:47 +0700

    refactoring

---
+++ src/LogsearchShipper.Core/ConfigWatcher.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using log4net;
+
+namespace LogSearchShipper.Core
+{
+	class ConfigWatcher : IDisposable
+	{
+		public ConfigWatcher(string fullPath, ILog log)
+		{
+			_log = log;
+			Watcher = new FileSystemWatcher(Path.GetDirectoryName(fullPath), Path.GetFileName(fullPath));
+			Watcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite | NotifyFilters.Size;
+		}
+
+		public DateTime LastWriteTime;
+		public readonly FileSystemWatcher Watcher;
+		public event Action Changed;
+
+		public void SubscribeConfigFileChanges(Action actionsToRun)
+		{
+			Watcher.Changed += (s, e) =>
+			{
+				try
+				{
+					_log.InfoFormat("Detected change in file: {0}", e.FullPath);
+					actionsToRun();
+				}
+				catch (Exception exc)
+				{
+					_log.Error(exc);
+				}
+			};
+			Watcher.EnableRaisingEvents = true;
+		}
+
+		public void Dispose()
+		{
+			Watcher.EnableRaisingEvents = false;
+			Watcher.Dispose();
+		}
+
+		private readonly ILog _log;
+	}
+}
