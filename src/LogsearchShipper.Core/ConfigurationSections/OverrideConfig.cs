473eb3a1609ee1f001933c7b6fbfa2673bfe007c
fandrei <andrei.faber@gmail.com>
2015-03-20 15:32:06 +0700

    added config override elements

---
+++ src/LogsearchShipper.Core/ConfigurationSections/OverrideConfig.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace LogSearchShipper.Core.ConfigurationSections
+{
+	public class OverrideConfig : ConfigurationElement
+	{
+		[ConfigurationProperty("forServiceNames", IsRequired = true, IsKey = true)]
+		public string ForServiceNames
+		{
+			get { return (string)this["forServiceNames"]; }
+			set { this["forServiceNames"] = value; }
+		}
+
+		[ConfigurationProperty("customNxLogConfig")]
+		public CustomNxlogConfig CustomNxlogConfig
+		{
+			get { return (CustomNxlogConfig)this["customNxLogConfig"]; }
+			set { this["customNxLogConfig"] = value; }
+		}
+
+		[ConfigurationProperty("closeWhenIdle", IsRequired = false, DefaultValue = true)]
+		public bool CloseWhenIdle
+		{
+			get { return (bool)this["closeWhenIdle"]; }
+			set { this["closeWhenIdle"] = value; }
+		}
+	}
+
+	[ConfigurationCollection(typeof(OverrideConfig), AddItemName = "overrideConfig")]
+	public class OverrideConfigCollection : ConfigurationElementCollection
+	{
+		protected override ConfigurationElement CreateNewElement()
+		{
+			return new OverrideConfig();
+		}
+
+		protected override object GetElementKey(ConfigurationElement element)
+		{
+			return ((OverrideConfig)element).ForServiceNames;
+		}
+	}
+}
