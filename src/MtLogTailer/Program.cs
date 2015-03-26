dbdcfa13bf99e1beccda5a0943f7dd8adaa58c0d
fandrei <andrei.faber@gmail.com>
2015-03-26 15:58:55 +0700

    added a simple MT log tailer

---
+++ src/MtLogTailer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MtLogTailer
+{
+	static class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				if (args.Length != 1)
+					throw new ApplicationException("Invalid args");
+				var filePath = args[0];
+
+				var shipper = new LogShipper(filePath);
+
+				Console.CancelKeyPress +=
+					(sender, eventArgs) =>
+					{
+						shipper.Stop();
+						eventArgs.Cancel = true;
+					};
+
+				shipper.Process();
+			}
+			catch (ApplicationException exc)
+			{
+				Console.WriteLine(exc.Message);
+			}
+			catch (Exception exc)
+			{
+				Console.WriteLine(exc);
+			}
+		}
+	}
+}
