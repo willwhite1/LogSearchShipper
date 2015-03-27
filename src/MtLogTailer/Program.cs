6cd0ab07ef74fa7f987362a6d215642f7a906af0
David Laing <david@davidlaing.com>
2015-03-27 09:31:53 +0000

    Allow encoding to be passed from command line (defaulting to 936 - gb2312 ANSI/OEM Simplified Chinese (PRC, Singapore); Chinese Simplified (GB2312) )

--- src/MtLogTailer/Program.cs
+++ src/MtLogTailer/Program.cs
@@ -12,10 +12,14 @@ namespace MtLogTailer
 			try
 			{
 				if (args.Length != 1)
-					throw new ApplicationException("Invalid args");
+					throw new ApplicationException("Invalid args.  Should use MtLogTailer.exe <filePath> <encoding: optional>");
 				var filePath = args[0];
 
-				var shipper = new LogShipper(filePath);
+				var encoding = 936;  //Default to gb2312-> ANSI/OEM Simplified Chinese (PRC, Singapore); Chinese Simplified (GB2312)
+			  if (args.Length >= 2)
+					encoding = Convert.ToInt32(args[1]);
+
+				var shipper = new LogShipper(filePath, encoding);
 
 				Console.CancelKeyPress +=
 					(sender, eventArgs) =>
