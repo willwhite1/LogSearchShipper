9ecbc486e8b1acf7ee94cfbee29eaa42a493c2bb
fandrei <andrei.faber@gmail.com>
2015-03-31 11:00:52 +0700

    process wildcard log names

---
+++ src/MtLogTailer/PathWatcher.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace MtLogTailer
+{
+	class PathWatcher
+	{
+		public PathWatcher(string path, int encoding)
+		{
+			_path = path;
+			_encoding = encoding;
+		}
+
+		public void Process()
+		{
+			while (true)
+			{
+				var dirPath = Directory.GetParent(_path).FullName;
+				var fileName = Path.GetFileName(_path);
+
+				foreach (var file in Directory.GetFiles(dirPath, fileName))
+				{
+					if (_terminate)
+						return;
+
+					LogShipper shipper;
+					if (!_shippers.TryGetValue(file, out shipper))
+					{
+						shipper = new LogShipper(file, _encoding);
+						_shippers.Add(file, shipper);
+					}
+
+					shipper.Process();
+				}
+
+				Thread.Sleep(TimeSpan.FromSeconds(1));
+			}
+		}
+
+		public void Stop()
+		{
+			_terminate = true;
+		}
+
+		private readonly Dictionary<string, LogShipper> _shippers = new Dictionary<string, LogShipper>();
+
+		private volatile bool _terminate;
+
+		private readonly string _path;
+		private readonly int _encoding;
+	}
+}
