99758a62af48fbf57806c723cc0d518dd3d63fbb
fandrei <andrei.faber@gmail.com>
2015-04-17 16:31:07 +0700

    don't stop on shipping or file access error

--- src/MtLogTailer/PathWatcher.cs
+++ src/MtLogTailer/PathWatcher.cs
@@ -31,16 +31,23 @@ namespace MtLogTailer
 			{
 				foreach (var file in Directory.GetFiles(dirPath, fileName, SearchOption.AllDirectories))
 				{
-					LogShipper shipper;
-					if (!_shippers.TryGetValue(file, out shipper))
+					try
 					{
-						shipper = new LogShipper(file, _encoding);
-						_shippers.Add(file, shipper);
-						if (_readFromLast && isFirstRead)
-							shipper.Update();
-					}
+						LogShipper shipper;
+						if (!_shippers.TryGetValue(file, out shipper))
+						{
+							shipper = new LogShipper(file, _encoding);
+							_shippers.Add(file, shipper);
+							if (_readFromLast && isFirstRead)
+								shipper.Update();
+						}
 
-					shipper.Process();
+						shipper.Process();
+					}
+					catch (Exception exc)
+					{
+						Program.LogError(exc.ToString());
+					}
 				}
 
 				isFirstRead = false;
