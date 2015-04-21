8a83a6ec56b6727f75831ef191bb5ce9d525cc0c
fandrei <andrei.faber@gmail.com>
2015-04-21 13:49:43 +0700

    fix #91 - setting sourceTailer from overrideConfig

--- src/LogsearchShipper.Core.Tests/EDBFileWatchParserTests.cs
+++ src/LogsearchShipper.Core.Tests/EDBFileWatchParserTests.cs
@@ -99,6 +99,7 @@ namespace LogSearchShipper.Core.Tests
 				Assert.AreEqual(2, matchedItems.Count());
 				Assert.IsTrue(matchedItems.All(item => item.CloseWhenIdle == false));
 				Assert.IsTrue(matchedItems.All(item => item.CustomNxlogConfig.Value == overrideConfig.CustomNxlogConfig.Value));
+				Assert.IsTrue(matchedItems.All(item => item.SourceTailer == TailerType.MT));
 
 				var notMatchedItems = fileWatchItems.ToList();
 				foreach (var item in matchedItems)
@@ -109,6 +110,7 @@ namespace LogSearchShipper.Core.Tests
 				Assert.IsTrue(notMatchedItems.Count > 0);
 				Assert.IsTrue(notMatchedItems.All(item => item.CloseWhenIdle == true));
 				Assert.IsTrue(notMatchedItems.All(item => string.IsNullOrEmpty(item.CustomNxlogConfig.Value)));
+				Assert.IsTrue(notMatchedItems.All(item => item.SourceTailer == TailerType.Normal));
 			}
 		}
 	}
