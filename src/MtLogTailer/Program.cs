0a4fa31da50828c2f91002c89b349197bf984f7a
Andrei Faber <andrei.faber@gmail.com>
2015-05-20 11:14:51 +0700

    don't log MT tailer exceptions to a file

--- src/MtLogTailer/Program.cs
+++ src/MtLogTailer/Program.cs
@@ -6,21 +6,16 @@ using System.Reflection;
 using System.Text;
 using System.Threading;
 
-using log4net;
-using log4net.Appender;
-using log4net.Config;
-using log4net.Layout;
-
 namespace MtLogTailer
 {
 	static class Program
 	{
 		static void Main(string[] args)
 		{
-			ConfigureLogging();
-
 			try
 			{
+				InitLogging();
+
 				var argsDic = CommandLineUtil.ParseArgs(args);
 
 				string path;
@@ -73,8 +68,8 @@ namespace MtLogTailer
 		{
 			try
 			{
-				var message = _version + " " + string.Format(format, args);
-				Logger.Error(message);
+				var message = string.Format("{0} {1} {2}", FormatTime(), _version, string.Format(format, args));
+				Console.WriteLine(message);
 			}
 			catch (Exception exc)
 			{
@@ -92,39 +87,19 @@ namespace MtLogTailer
 			LogErrorFormat(Escape(message));
 		}
 
-		private static void ConfigureLogging()
+		static string FormatTime()
 		{
-			var pattern = "%utcdate{ISO8601}Z %message%newline";
+			return DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ss.fffffffZ");
+		}
 
-			var layout = new PatternLayout(pattern);
-			layout.ActivateOptions();
-
-			var appender1 = new ConsoleAppender
-			{
-				Layout = layout,
-			};
-			appender1.ActivateOptions();
-
-			var appender2 = new RollingFileAppender
-			{
-				Layout = layout,
-				File = "MtLogTailer.log",
-				AppendToFile = true,
-				RollingStyle = RollingFileAppender.RollingMode.Size,
-				MaximumFileSize = "1MB",
-				MaxSizeRollBackups = 2,
-			};
-			appender2.ActivateOptions();
-
-			BasicConfigurator.Configure(appender1, appender2);
-
+		static void InitLogging()
+		{
 			var assembly = Assembly.GetExecutingAssembly();
 			var version = FileVersionInfo.GetVersionInfo(assembly.Location);
 			_version = string.Format("{0} {1}", version.OriginalFilename, version.FileVersion);
 		}
 
 		public static volatile bool Terminate;
-		private static readonly ILog Logger = LogManager.GetLogger(typeof(Program));
 		private static string _version;
 	}
 }
