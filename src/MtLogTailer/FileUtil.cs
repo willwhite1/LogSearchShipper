986a159a29fe6972066b9c0bdac2706670a60014
Andrei Faber <andrei.faber@gmail.com>
2015-06-04 15:05:48 +0700

    refactoring - encoding detection

--- src/MtLogTailer/FileUtil.cs
+++ src/MtLogTailer/FileUtil.cs
@@ -10,14 +10,17 @@ namespace MtLogTailer
 	{
 		// Simple detection of stream encoding
 		// see also http://stackoverflow.com/questions/4520184/how-to-detect-the-character-encoding-of-a-text-file
-		public static Encoding DetectEncoding(Stream stream)
+		public static Encoding DetectEncoding(Stream stream, Encoding defaultEncoding)
 		{
 			var startPosition = stream.Position;
 
 			var bom = new byte[4];
 			var bytesRead = stream.Read(bom, 0, 4);
 			if (bytesRead < bom.Length)
+			{
+				stream.Position = startPosition;
 				return null;
+			}
 
 			if (bom[0] == 0x2b && bom[1] == 0x2f && bom[2] == 0x76)
 			{
@@ -44,44 +47,9 @@ namespace MtLogTailer
 			if (bom[0] == 0 && bom[1] == 0 && bom[2] == 0xfe && bom[3] == 0xff)
 				return Encoding.UTF32;
 
-			// UTF-8 without BOM can give false positives
-			var encoding = Encoding.UTF8;
-			var nonAsciiCharsFound = 0;
-			try
-			{
-				var bufStream = new BufferedStream(stream);
-				var reader = new BinaryReader(bufStream, encoding);
-				while (true)
-				{
-					var ch = reader.Read();
-					if (ch == -1)
-						break;
-					if (ch == (char)0xFFFD)
-					{
-						encoding = Encoding.Default;
-						break;
-					}
-					if (ch > 0x80)
-						nonAsciiCharsFound++;
-					if (nonAsciiCharsFound > 16)
-						break;
-				}
-			}
-			catch (ArgumentOutOfRangeException)
-			{
-				encoding = Encoding.Default;
-			}
-			catch (ArgumentException)
-			{
-				encoding = Encoding.Default;
-			}
-			catch (EndOfStreamException)
-			{
-				encoding = Encoding.Default;
-			}
-
 			stream.Position = startPosition;
-			return encoding;
+
+			return defaultEncoding;
 		}
 	}
 }
