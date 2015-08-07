05e22fbc5a9d1071e2f4ba2a56660cc0ff8ca630
fandrei <andrei.faber@gmail.com>
2015-08-07 16:47:39 +0700

    extra test - EDB config file update that doesn't affect nxlog config

--- src/LogsearchShipper.Core.Tests/NxLog/NxlogRestartingTest.cs
+++ src/LogsearchShipper.Core.Tests/NxLog/NxlogRestartingTest.cs
@@ -55,9 +55,26 @@ namespace LogSearchShipper.Core.Tests.NxLog
 			{
 				var edbConfig = lssManager.LogSearchShipperConfig.EDBFileWatchers[0].DataFile;
 
+				// test restarting when the config file last write time is updated without changing content
 				File.SetLastWriteTime(edbConfig, DateTime.Now);
 
 				// give the LSS manager some time to restart the nxlog service
+				Thread.Sleep(TimeSpan.FromSeconds(10));
+
+				Assert.IsTrue(nxLogProcessId == lssManager.NxLogProcessManager.NxLogProcess.Id);
+
+				// test restarting when a config file update doesn't affect the nxlog config
+				var config = new XmlDocument();
+				config.Load(edbConfig);
+
+				var nodes = config.SelectNodes("/Environment");
+				foreach (XmlElement node in nodes)
+				{
+					node.InsertAfter(config.CreateElement("TEST_UPDATE"), node.ChildNodes[0]);
+				}
+
+				config.Save(edbConfig);
+
 				Thread.Sleep(TimeSpan.FromSeconds(10));
 
 				Assert.IsTrue(nxLogProcessId == lssManager.NxLogProcessManager.NxLogProcess.Id);
