e49470c0875f5cdb088f8b08bd9b9c15eadc0d89
Andrei Faber <andrei.faber@gmail.com>
2015-09-22 14:51:21 +0700

    developing - auto update

---
+++ src/LogSearchShipper/Const.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogSearchShipper
+{
+	static class Const
+	{
+		public const string AppName = "LogSearchShipper";
+
+		public static string WorkingAreaPath
+		{
+			get
+			{
+				var basePath = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+				var res = Path.Combine(basePath, "City Index", AppName);
+				return res;
+			}
+		}
+
+		public static string UpdateAreaPath
+		{
+			get { return Path.Combine(WorkingAreaPath, "Update"); }
+		}
+	}
+}
