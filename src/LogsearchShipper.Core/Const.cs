13855dd363024de609116d6dd7d7f778bc11fbd0
Andrei Faber <andrei.faber@gmail.com>
2015-09-24 11:47:34 +0700

    refactoring

--- src/LogsearchShipper.Core/Const.cs
+++ src/LogsearchShipper.Core/Const.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LogSearchShipper.Updater
+namespace LogSearchShipper.Core
 {
 	public static class Const
 	{
