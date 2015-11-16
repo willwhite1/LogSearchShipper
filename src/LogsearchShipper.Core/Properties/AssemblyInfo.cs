26490d086f31fc9538bf32bd6bafc89a5a90fb75
Andrei Faber <andrei.faber@gmail.com>
2015-11-16 16:04:48 +0700

    added AssemblyInformationalVersion

--- src/LogsearchShipper.Core/Properties/AssemblyInfo.cs
+++ src/LogsearchShipper.Core/Properties/AssemblyInfo.cs
@@ -17,6 +17,8 @@ using System.Runtime.CompilerServices;
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
 [assembly: AssemblyVersion("1.13.*")]
+[assembly: AssemblyInformationalVersion("")]
+
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
 //[assembly: AssemblyDelaySign(false)]
