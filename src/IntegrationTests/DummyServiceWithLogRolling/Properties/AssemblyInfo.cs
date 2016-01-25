6554364dd41742570da01d857b5e6bccc2615b04
Andrei Faber <andrei.faber@gmail.com>
2016-01-25 12:27:03 +0700

    cleanup

--- src/IntegrationTests/DummyServiceWithLogRolling/Properties/AssemblyInfo.cs
+++ src/IntegrationTests/DummyServiceWithLogRolling/Properties/AssemblyInfo.cs
@@ -24,16 +24,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("0daebe3f-b9a0-43e1-8f06-9139c6054815")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- src/IntegrationTests/Properties/AssemblyInfo.cs
+++ src/IntegrationTests/Properties/AssemblyInfo.cs
@@ -24,16 +24,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("817893f8-403c-4953-ab68-26b13ebdd1d3")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyInformationalVersion("")]
