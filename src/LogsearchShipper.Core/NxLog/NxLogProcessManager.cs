9356ca4ffe155c1895895ce4b41fec489417e6c1
Kevin Holditch <kevholditch@hotmail.com>
2016-08-31 08:52:27 +0100

    removed shipping to logz for now going to junction at amazon

--- src/LogsearchShipper.Core/NxLog/NxLogProcessManager.cs
+++ src/LogsearchShipper.Core/NxLog/NxLogProcessManager.cs
@@ -485,7 +485,7 @@ SpoolDir	{6}
 	WarnLimit 50000
 </Processor>
 <Route route_to_syslog>
-	Path {0} => buffer_out_syslog => out_syslog => out_logz
+	Path {0} => buffer_out_syslog => out_syslog
 </Route>
 ", allInputs);
             }
@@ -566,8 +566,8 @@ rM8ETzoKmuLdiTl3uUhgJMtdOP8w7geYl8o1YP+3YQ==
 	Module	om_ssl
 	Host	{0}
 	Port	{1}
-	AllowUntrusted TRUE
-	Exec	if $Message $Message=replace($Message,""\n"",""¬"");
+	AllowUntrusted TRUE    
+	Exec	$message=$raw_event; if $Message $Message=replace($Message,""\n"",""¬"");
 	Exec	to_syslog_ietf();
 </Output>",
                 OutputSyslog.Host, OutputSyslog.Port);
@@ -575,6 +575,7 @@ rM8ETzoKmuLdiTl3uUhgJMtdOP8w7geYl8o1YP+3YQ==
 
         private string GenerateOutputLogzConfig()
         {
+            return string.Empty;
             var certFile = Path.Combine(DataFolder, @"logz.crt");
             File.WriteAllText(certFile, @"-----BEGIN CERTIFICATE-----
 MIICsDCCAhmgAwIBAgIJAJZZlYOII804MA0GCSqGSIb3DQEBBQUAMEUxCzAJBgNV
