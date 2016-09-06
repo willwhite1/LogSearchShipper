1928c957b66590b002bbe2fd1bf7d536d0ccb4e1
Sam Owens <sam.owens@cityindex.co.uk>
2016-09-06 10:26:18 +0100

    added env vars

--- src/LogSearchShipper/Install.bat
+++ src/LogSearchShipper/Install.bat
@@ -4,6 +4,10 @@ IF NOT EXIST current GOTO MKLINK
 rmdir current
 :MKLINK
 mklink /j current 1.13.260
+
+SETX /m EnvironmentType LIVE
+SETX /m EnvironmentName PROD-DESKTOP
+
 .\current\LogSearchShipper.exe install
 net start LogSearchShipper
 Pause
